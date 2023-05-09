4c1b5da58beea97167c76ab81e4d38004b10add1
Suyash211998 <suyashdudwadkar@gmail.com>
2023-05-09 20:02:13 +0530

    Just Started

---
+++ src/main/java/com/oorja/ProductBasedApplication/ProductBasedApplication.java
@@ -0,0 +1,13 @@
+package com.oorja.ProductBasedApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductBasedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductBasedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oorja/ProductBasedApplication/ProductBasedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oorja.ProductBasedApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductBasedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
